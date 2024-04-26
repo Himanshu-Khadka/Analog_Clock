253a2f01af77b1f0744386a54fe50ee53f902b47
Himanshu-Khadka <iamhimanshu.me@gmail.com>
2024-04-26 01:04:53 -0400

    testing Draw Circle

--- src/main/java/himanshukhadka/analogclock/DisplayClock.java
+++ src/main/java/himanshukhadka/analogclock/DisplayClock.java
@@ -1,11 +1,19 @@
 package himanshukhadka.analogclock;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class DisplayClock extends Application {
     @Override
     public void start(Stage stage) throws Exception {
+        BorderPane mainPane = new BorderPane();
+        mainPane.setCenter(new DrawClock());
 
+        Scene scene = new Scene(mainPane,500,500);
+        stage.setTitle("Analog Clock");
+        stage.setScene(scene);
+        stage.show();
     }
 }
