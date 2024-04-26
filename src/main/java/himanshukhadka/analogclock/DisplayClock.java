504cd273d6e96b77d17ef03749ef78b68cf90ab5
Himanshu-Khadka <iamhimanshu.me@gmail.com>
2024-04-26 00:55:53 -0400

    initial files

---
+++ src/main/java/himanshukhadka/analogclock/DisplayClock.java
@@ -0,0 +1,11 @@
+package himanshukhadka.analogclock;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class DisplayClock extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
---
+++ src/main/java/himanshukhadka/analogclock/DrawClock.java
@@ -0,0 +1,17 @@
+package himanshukhadka.analogclock;
+
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
+public class DrawClock extends Pane {
+    public DrawClock(){
+        Circle circle = new Circle();
+        circle.centerXProperty().bind(widthProperty().divide(2).subtract(50));
+        circle.centerYProperty().bind(heightProperty().divide(2).subtract(50));
+        circle.setStroke(Color.BLACK);
+        circle.setStrokeWidth(4);
+        circle.setFill(Color.WHITE);
+
+    }
+}
