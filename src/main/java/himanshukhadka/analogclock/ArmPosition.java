d34cf600752720b6a9cabdff4c9e010fec74e4b7
Himanshu-Khadka <iamhimanshu.me@gmail.com>
2024-04-26 13:01:57 -0400

    additional code

--- src/main/java/himanshukhadka/analogclock/ArmPosition.java
+++ src/main/java/himanshukhadka/analogclock/ArmPosition.java
@@ -1,5 +1,28 @@
 package himanshukhadka.analogclock;
 
+import java.util.GregorianCalendar;
+
 public class ArmPosition {
 
+    //Creating time
+    GregorianCalendar calnder = new GregorianCalendar();
+
+    public double HOUR = calnder.HOUR;
+    public double MINUTE = calnder.MINUTE;
+    public double SECOND = calnder.SECOND;
+
+    public double getHour(){
+        return HOUR;
+    }
+    public double getMinute(){
+        return MINUTE;
+    }
+    public double getSecond(){
+        return SECOND;
+    }
+
+    @Override
+    public String toString() {
+        return HOUR +":"+MINUTE+":"+SECOND;
+    }
 }
--- src/main/java/himanshukhadka/analogclock/DisplayClock.java
+++ src/main/java/himanshukhadka/analogclock/DisplayClock.java
@@ -3,13 +3,24 @@ package himanshukhadka.analogclock;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class DisplayClock extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         BorderPane mainPane = new BorderPane();
         mainPane.setCenter(new DrawClock());
+
+        //Creating ArmPosition class
+        Calendar calander = new GregorianCalendar();
+        String timeString = calander.get(Calendar.HOUR_OF_DAY)+":"+calander.get(Calendar.MINUTE)+":"+calander.get(Calendar.SECOND);
+
+        Text timeInText = new Text(timeString);
+        mainPane.setBottom(timeInText);
 
         Scene scene = new Scene(mainPane,500,500);
         stage.setTitle("Analog Clock");
