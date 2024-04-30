781cf8fe65e792d34ad6a51afba3621bd78b9487
Himanshu-Khadka <iamhimanshu.me@gmail.com>
2024-04-29 23:47:34 -0600

    testing

--- src/main/java/himanshukhadka/analogclock/DrawClock.java
+++ src/main/java/himanshukhadka/analogclock/DrawClock.java
@@ -5,26 +5,51 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 
+import java.awt.*;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class DrawClock extends Pane {
+
+    public int second;
+    public int minute;
+    public int hour;
+
+    Calendar calendar = new GregorianCalendar();
+
+    public void setTime(){
+        this.hour = calendar.get(GregorianCalendar.HOUR_OF_DAY);
+        this.minute = calendar.get(GregorianCalendar.MINUTE);
+        this.second = calendar.get(GregorianCalendar.SECOND);
+    }
+
+    public void setSecond(int second){
+        this.second = second;
+    }
+
+    public void setMinute(int minute){
+        this.minute = minute;
+    }
+
+    public void setHour(int hour){
+        this.hour = hour;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public int getMinute(){
+        return minute;
+    }
+
+    public int getSecond(){
+        return second;
+    }
+
+
+
     public DrawClock(){
-        Circle circle = new Circle();
-        circle.centerXProperty().bind(widthProperty().divide(2));
-        circle.centerYProperty().bind(heightProperty().divide(2));
-        circle.setRadius(200);
-        circle.setStroke(Color.BLACK);
-        circle.setStrokeWidth(4);
-        circle.setFill(Color.WHITE);
-
-        Line secondArm = new Line();
-        secondArm.startXProperty().bind(widthProperty().divide(2));
-        secondArm.startYProperty().bind(heightProperty().divide(2));
-        secondArm.setStrokeWidth(3);
-        secondArm.setStroke(Color.BLACK);
-
-
-
-        getChildren().addAll(circle,secondArm);
-
 
     }
 }
