f16c58f0eb2abb09caa0cac6ab44d8b1200170d7
Himanshu-Khadka <iamhimanshu.me@gmail.com>
2024-04-30 00:38:43 -0600

    some more fix

--- src/main/java/himanshukhadka/analogclock/DrawClock.java
+++ src/main/java/himanshukhadka/analogclock/DrawClock.java
@@ -5,7 +5,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 
-import java.awt.*;
+
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
@@ -50,6 +50,43 @@ public class DrawClock extends Pane {
 
 
     public DrawClock(){
+        double armLength = Math.min(getHeight(), getWidth()) * 0.8 * 0.5;
 
+        double centerX = getWidth()/2;
+        double centerY = getHeight()/2;
+
+        Circle mainCircle = new Circle();
+        mainCircle.setCenterX(centerX);
+        mainCircle.setCenterY(centerY);
+        mainCircle.setRadius(200);
+        mainCircle.setFill(Color.WHITE);
+        mainCircle.setStroke(Color.BLACK);
+        mainCircle.setStrokeWidth(5);
+
+        Line secondArm = new Line();
+        secondArm.setStartX(centerX);
+        secondArm.setStartY(centerY);
+        secondArm.setStroke(Color.BLACK);
+        secondArm.setStrokeWidth(3);
+        secondArm.setEndX(centerX+(armLength*Math.sin(getSecond()*(2*Math.PI)/60)));
+        secondArm.setEndY(centerY-(armLength*Math.cos((getSecond())*(2*Math.PI)/60)));
+
+        Line minuteArm = new Line();
+        minuteArm.setStartX(centerX);
+        minuteArm.setStartY(centerY);
+        minuteArm.setStroke(Color.BLACK);
+        minuteArm.setStrokeWidth(5);
+        minuteArm.setEndX(centerX+(armLength+Math.sin((getSecond()/60 + getMinute())*(2*Math.PI)/60)));
+        minuteArm.setEndY(centerY-(armLength*Math.cos((getSecond()/60 + getMinute())*(2*Math.PI)/60)));
+
+        Line hourArm = new Line();
+        hourArm.setStartX(centerX);
+        hourArm.setStartY(centerY);
+        hourArm.setStroke(Color.BLACK);
+        hourArm.setStrokeWidth(5);
+        hourArm.setEndX(centerX+(armLength+Math.sin((getSecond()/60 + getMinute()/60 + getHour())*(2*Math.PI)/60)));
+        hourArm.setEndY(centerY-(armLength*Math.cos((getSecond()/60 + getMinute()/60 + getHour())*(2*Math.PI)/60)));
+
+        getChildren().addAll(mainCircle,secondArm,minuteArm,hourArm);
     }
 }
